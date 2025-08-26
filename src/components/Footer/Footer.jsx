c5e09ee913d6ef10233f05005b78b74f0fd6785d
adam wainkainen <awainikainen@gmail.com>
2025-08-25 21:19:04 -0400

    footer for jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h3 className="footer__content">Developed by Adam Wainikainen</h3>
+      <span className="footer__year">2025</span>
+    </footer>
+  );
+}
+
+export default Footer;
