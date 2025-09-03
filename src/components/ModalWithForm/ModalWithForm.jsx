4c4db228a49b6784acefbc8b4de2c9b9725fade0
adam wainkainen <awainikainen@gmail.com>
2025-09-03 01:23:21 -0400

    :ambulance:

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,6 +1,6 @@
 //had to have this file for robot check will remove after
-import React from 'react'
-
+import React from 'react';
+import 'ModalWithForm.css';
 const ModalWithForm = () => {
   return (
     <div>ModalWithForm</div>
