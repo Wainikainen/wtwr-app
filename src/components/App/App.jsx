196474790d5f7992d4d794124fb2d2c9c7a4c10f
adam wainkainen <awainikainen@gmail.com>
2025-09-03 01:08:31 -0400

    added handles for temp scale changes and use effect for weather data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
 import FormModal from "../FormModal/FormModal";
 import ItemModal from "../ItemModal/ItemModal";
 import { defaultClothingItems } from "../../utils/defaultClothingItems";
+import { getWeatherData } from "../../utils/weatherApi";
 import "./App.css";
+import CurrentTempScaleContext from "../../contexts/CurrentTempScaleContext";
 
 function App() {
-  const [clothingItems, setClothingItems] = useState(defaultClothingItems);
+  const [clothingItems, setClothingItems] = useState([]);
   const [activeModal, setActiveModal] = useState("");
   const [selectedCard, setSelectedCard] = useState({});
+  const [weatherData, setWeatherData] = useState({ name: "", temp: "0" });
+  const [tempScale, setTempScale] = useState("F");
 
   function handleOpenItemModal(card) {
     setActiveModal("item-modal");
@@ -25,24 +29,50 @@ function App() {
     setActiveModal("");
   }
 
+  function handleScaleChange() {
+    if (tempScale === "F") {
+      setTempScale("C");
+    } else {
+      setTempScale("F");
+    }
+  }
+
+  useEffect(() => {
+    getWeatherData()
+      .then((data) => {
+        setWeatherData(data);
+      })
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    setClothingItems(defaultClothingItems);
+  }, []);
+
   return (
-    <div className="app">
-      <Header handleAddGarmentModal={handleAddGarmentModal} />
-      <Main
-        clothingItems={clothingItems}
-        handleOpenItemModal={handleOpenItemModal}
-      />
-      <Footer />
-      <ItemModal
-        card={selectedCard}
-        isOpen={activeModal === "item-modal"}
-        onClose={handleCloseModal}
-      />
-      <FormModal
-        isOpen={activeModal === "add-garment-modal"}
-        onClose={handleCloseModal}
-      ></FormModal>
-    </div>
+    <CurrentTempScaleContext.Provider value={{ tempScale, handleScaleChange }}>
+      <div className="app">
+        <Header
+          weatherData={weatherData}
+          handleAddGarmentModal={handleAddGarmentModal}
+        />
+        <Main
+          weatherData={weatherData}
+          clothingItems={clothingItems}
+          handleOpenItemModal={handleOpenItemModal}
+        />
+        <Footer />
+        <ItemModal
+          card={selectedCard}
+          isOpen={activeModal === "item-modal"}
+          onClose={handleCloseModal}
+        />
+        <FormModal
+          isOpen={activeModal === "add-garment-modal"}
+          onClose={handleCloseModal}
+        ></FormModal>
+      </div>
+    </CurrentTempScaleContext.Provider>
   );
 }
 
