e15da321efada0acb6d1b41f65e03272fff80d34
adam wainkainen <awainikainen@gmail.com>
2025-09-02 22:04:35 -0400

    added api and parsed data functions and render rules

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,15 +1,46 @@
-import bannerImg from "../../images/weatherCard.svg";
+import cloudyDay from "../../images/cloudy-day.svg";
+import cloudyNight from "../../images/cloudy-night.svg";
+import fogDay from "../../images/fog-day.svg";
+import fogNight from "../../images/fog-night.svg";
+import rainDay from "../../images/rain-day.svg";
+import rainNight from "../../images/rain-night.svg";
+import snowDay from "../../images/snow-day.svg";
+import snowNight from "../../images/snow-night.svg";
+import stormDay from "../../images/storm-day.svg";
+import stormNight from "../../images/storm-night.svg";
+import sunnyDay from "../../images/sunny-day.svg";
+import sunnyNight from "../../images/sunny-night.svg";
 import "./WeatherCard.css";
+import { useContext } from "react";
+import CurrentTempScaleContext from "../../contexts/CurrentTempScaleContext";
 
-function WeatherCard() {
+function WeatherCard({ weatherData }) {
+  const { tempScale } = useContext(CurrentTempScaleContext);
+  const now = Date.now() / 1000;
+  const isDay = now >= weatherData.sunrise && now <= weatherData.sunset;
+ 
+
+
+
+
+
+
+
+
+
+
+  
+
   return (
     <div className="weather__card">
       <img
-        src={bannerImg}
+        src={weatherImage}
         className="weather__banner-img"
         alt="Weather Banner"
       />
-      <p className="weather__card-txt">70°</p>
+      <p className="weather__card-txt">
+        {weatherData.temp[tempScale]}&deg; {tempScale}
+      </p>
     </div>
   );
 }
---
+++ src/utils/weatherApi.js
@@ -0,0 +1,32 @@
+import { apiKey, coordinates } from "./constants";
+
+export function getWeatherData() {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&units=imperial&appid=${apiKey}`
+  )
+    .then((res) => {
+      return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    })
+    .then((data) => {
+      return readWeatherData(data);
+    });
+}
+
+function readWeatherData(data) {
+  const readData = { temp: {} };
+  readData.city = data.name;
+  readData.temp.F = Math.round(data.main.temp);
+  readData.temp.C = Math.round(((data.main.temp - 32) * 5) / 9);
+  readData.condition = getWeatherCondition(readData.temp.F);
+  return readData;
+}
+
+function getWeatherCondition(tempF) {
+  if (tempF >= 86) {
+    return "hot";
+  } else if (tempF >= 66 && tempF < 86) {
+    return "warm";
+  } else {
+    return "cold";
+  }
+}
