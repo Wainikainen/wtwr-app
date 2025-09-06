3d385358eff81cdbf7dc52c0df30c331c48120e9
adam wainkainen <awainikainen@gmail.com>
2025-09-06 13:42:57 -0400

    added delete for card

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,6 +1,6 @@
 import "./ItemModal.css";
 
-function ItemModal({ card, isOpen, onClose }) {
+function ItemModal({ card, isOpen, onClose, handleDeleteModal }) {
   return (
     <div className={`item__modal ${isOpen ? "modal_is-opened" : ""}`}>
       <div className="item__modal-container">
@@ -9,10 +9,17 @@ function ItemModal({ card, isOpen, onClose }) {
           className="item__modal-close"
           onClick={onClose}
         ></button>
-        <img src={card.link} alt={card.name} className="item__modal-img" />
+        <img src={card.imageUrl} alt={card.name} className="item__modal-img" />
         <div className="item__modal-footer">
           <h2 className="item__modal-title">{card.name}</h2>
           <p className="item__modal-text">Weather: {card.weather}</p>
+          <button
+            className="item__modal-delete"
+            type="button"
+            onClick={() => handleDeleteModal(card)}
+          >
+            Delete Item
+          </button>
         </div>
       </div>
     </div>
