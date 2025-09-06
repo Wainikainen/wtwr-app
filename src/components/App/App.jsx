d7fe8ccc65fc978b2f1667c77c65f31b7abc2a2b
adam wainkainen <awainikainen@gmail.com>
2025-09-06 13:40:28 -0400

    added routes for profile and home, click listeners for outside modal, function for api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,22 @@
 import { useEffect, useState } from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Main from "../Main/Main";
-import FormModal from "../FormModal/FormModal";
-import ItemModal from "../ItemModal/ItemModal";
-import { defaultClothingItems } from "../../utils/defaultClothingItems";
-import { getWeatherData } from "../../utils/weatherApi";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CurrentTempScaleContext from "../../contexts/CurrentTempScaleContext";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import { defaultClothingItems } from "../../utils/defaultClothingItems";
+import { getWeatherData } from "../../utils/weatherApi";
+import Footer from "../Footer/Footer";
+import FormModal from "../FormModal/FormModal";
+import ItemModal from "../ItemModal/ItemModal";
+import Profile from "../Profile/Profile";
+import AddItemModal from "../AddItemModal/AddItemModal";
+import DeleteModal from "../OpenConfirmationModal/DeleteModal";
+import {
+  getClothingItems,
+  addClothingItem,
+  deleteClothingItem,
+} from "../../utils/api";
 
 function App() {
   const [clothingItems, setClothingItems] = useState([]);
@@ -25,6 +34,21 @@ function App() {
     setActiveModal("add-garment-modal");
   }
 
+  function handleDeleteModal(card) {
+    setActiveModal("delete-modal");
+    setSelectedCard(card);
+  }
+
+  function handleDelete() {
+    deleteClothingItem(selectedCard._id).then(() => {
+      const updatedClothingItems = clothingItems.filter(
+        (item) => item._id !== selectedCard._id
+      );
+      setClothingItems(updatedClothingItems);
+      handleCloseModal();
+    });
+  }
+
   function handleCloseModal() {
     setActiveModal("");
   }
@@ -37,6 +61,13 @@ function App() {
     }
   }
 
+  function handleClothingItems(newItem) {
+    addClothingItem(newItem).then((addedItem) => {
+      setClothingItems((prevItems) => [addedItem, ...prevItems]);
+      handleCloseModal();
+    });
+  }
+
   useEffect(() => {
     getWeatherData()
       .then((data) => {
@@ -46,8 +77,36 @@ function App() {
   }, []);
 
   useEffect(() => {
-    setClothingItems(defaultClothingItems);
+    getClothingItems().then((data) => setClothingItems(data));
   }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      const modalClass =
+        activeModal === "item-modal"
+          ? "item__modal"
+          : activeModal === "add-garment-modal"
+          ? "form__modal"
+          : "deleteModal";
+      const modal = document.querySelector(
+        `.${
+          activeModal === "item-modal"
+            ? "item__modal-container"
+            : activeModal === "add-garment-modal"
+            ? "form__modal-container" //for some reason i cant figure out why //
+            : "deleteModal__container" //form modal is not closing on click outside //
+        }`
+      );
+      if (
+        modal &&
+        e.target.closest(`.${modalClass}`) &&
+        !modal.contains(e.target)
+      )
+        handleCloseModal();
+    };
+    if (activeModal) document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, [activeModal]);
 
   return (
     <CurrentTempScaleContext.Provider value={{ tempScale, handleScaleChange }}>
@@ -56,21 +115,53 @@ function App() {
           weatherData={weatherData}
           handleAddGarmentModal={handleAddGarmentModal}
         />
-        <Main
-          weatherData={weatherData}
-          clothingItems={clothingItems}
-          handleOpenItemModal={handleOpenItemModal}
-        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Main
+                weatherData={weatherData}
+                clothingItems={clothingItems}
+                handleOpenItemModal={handleOpenItemModal}
+                handleDeleteModal={handleDeleteModal}
+              />
+            }
+          ></Route>
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                clothingItems={clothingItems}
+                handleOpenItemModal={handleOpenItemModal}
+                handleAddGarmentModal={handleAddGarmentModal}
+                handleDeleteModal={handleDeleteModal}
+              />
+            }
+          ></Route>
+        </Routes>
         <Footer />
         <ItemModal
           card={selectedCard}
           isOpen={activeModal === "item-modal"}
           onClose={handleCloseModal}
+          handleDeleteModal={handleDeleteModal}
         />
         <FormModal
           isOpen={activeModal === "add-garment-modal"}
           onClose={handleCloseModal}
-        ></FormModal>
+          handleSubmit={handleClothingItems}
+        />
+        <AddItemModal
+          isOpen={activeModal === "add-garment-modal"}
+          onClose={handleCloseModal}
+          handleSubmit={handleClothingItems}
+        ></AddItemModal>
+        <DeleteModal
+          card={selectedCard}
+          isOpen={activeModal === "delete-modal"}
+          onClose={handleCloseModal}
+          handleDelete={handleDelete}
+        />
       </div>
     </CurrentTempScaleContext.Provider>
   );
