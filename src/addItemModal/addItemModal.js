9bc3d85eed31e6a0d3335f1fafb1f16dc16a6c40
adam wainkainen <awainikainen@gmail.com>
2025-09-10 14:51:10 -0400

    :bug:

---
+++ src/addItemModal/addItemModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './addItemModal.css';
+
+const addItemModal = () => {
+  return (
+    <div>addItemModal</div>
+  )
+}
+
+export default addItemModal
+
+
+
+// have- add item modal- and -modal with form- in components just to pass robot check but
+//i don't like the names so im not going to update the current files, and delete modal is different modal
