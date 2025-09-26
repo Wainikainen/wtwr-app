ff21601d99e4973ccdafc3bbc3765fde63329779
adam wainkainen <awainikainen@gmail.com>
2025-09-26 18:13:57 -0400

    added sign out and edit profile modals

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import  { useContext, useEffect, useState } from "react";
 import "./SideBar.css";
-import userLogo from "../../images/avatar.svg";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-const SideBar = () => {
+const SideBar = ({handleSignOut, handleEditProfileModal}) => {
+  const { currentUser } = useContext(CurrentUserContext);
+  const [imageError, setImageError] = useState(false);
+
+  useEffect(() => {
+    setImageError(false);
+  }, [currentUser]);
+
   return (
     <aside className="sidebar">
       <div className="sidebar__row">
-        <p className="sidebar__user">Terrance</p>
-        <img src={userLogo} alt="USER IMAGE" className="sidebar__user-img" />
+        <p className="sidebar__user">{currentUser?.name}</p>
+        {imageError ? (
+          <div className="sidebar__img-error">
+            <p className="sidebar__img-error-name">{currentUser?.name?.charAt(0)}</p>
+          </div>
+        ) : (
+          <img
+            onError={() => {
+              setImageError(true);
+            }}
+            src={currentUser?.avatar}
+            alt="USER IMAGE"
+            className="sidebar__user-img"
+          />
+        )}
       </div>
+        <div className="sidebar__btns">
+          <button onClick={handleEditProfileModal}  className="sidebar__edit">Edit profile</button>
+          <button onClick={handleSignOut} className="sidebar__signOut">
+            Sign out
+          </button>
+        </div>
     </aside>
   );
 };
