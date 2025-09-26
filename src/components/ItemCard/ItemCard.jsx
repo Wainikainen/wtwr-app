68ba022928519ec8c6d5acbe66037538baa8821a
adam wainkainen <awainikainen@gmail.com>
2025-09-26 18:12:13 -0400

    added image error alt..and delete button protection

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,14 +1,19 @@
 import "./ItemCard.css";
+import DefaultImageError from "../../images/image-error.svg"
+import { useState } from "react";
 
 function ItemCard({ data, onCardClick }) {
+  const [imageError, setImageError] = useState(false);
   function handleOpenCard() {
     onCardClick(data);
   }
   return (
     <li className="card">
       <h2 className="card__title">{data.name}</h2>
-      <img
-        src={data.imageUrl}
+      <img onError={()=>{
+        setImageError(true)
+      }}
+        src={imageError ? DefaultImageError : data.imageUrl}
         alt={data.name}
         className="card__img"
         onClick={handleOpenCard}
--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,6 +1,11 @@
 import "./ItemModal.css";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
 function ItemModal({ card, isOpen, onClose, handleDeleteModal }) {
+  const { currentUser } = useContext(CurrentUserContext);
+  const isTheOwner = card.owner === currentUser?._id;
+
   return (
     <div className={`item__modal ${isOpen ? "modal_is-opened" : ""}`}>
       <div className="item__modal-container">
@@ -13,13 +18,15 @@ function ItemModal({ card, isOpen, onClose, handleDeleteModal }) {
         <div className="item__modal-footer">
           <h2 className="item__modal-title">{card.name}</h2>
           <p className="item__modal-text">Weather: {card.weather}</p>
-          <button
-            className="item__modal-delete"
-            type="button"
-            onClick={() => handleDeleteModal(card)}
-          >
-            Delete Item
-          </button>
+          {isTheOwner && (
+            <button
+              className="item__modal-delete"
+              type="button"
+              onClick={() => handleDeleteModal(card)}
+            >
+              Delete Item
+            </button>
+          )}
         </div>
       </div>
     </div>
