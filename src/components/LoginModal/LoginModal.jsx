029f900fcdcc3f811a1fc75d33e57507a38d4e45
adam wainkainen <awainikainen@gmail.com>
2025-09-26 18:14:05 -0400

    login modal

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,72 +1,84 @@
 import "./LoginModal.css";
-import { useState, useContext } from "react";
-import { validationImageUrl } from "../../utils/validation";
+import { useContext} from "react";
 import { useForm } from "../../hooks/useForm";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
-import { login } from "../../utils/auth";
+import { login, validateToken } from "../../utils/auth";
 
-function LoginModal({ isOpen, onClose, handleSubmit }) {
-  const {  resetForm, values, handleChange } = useForm({
-        email: "",
-        password: "",
+function LoginModal({ isOpen, onClose, handleRegister }) {
+  const { resetForm, values, handleChange,isValid, formRef } = useForm({
+    email: "",
+    password: "",
   });
 
-  const { setCurrentUser } = useContext(CurrentUserContext);
+  const { setCurrentUser, setIsLoggedIn } = useContext(CurrentUserContext);
+
+  const handleClose = () => {
+    resetForm();
+    onClose();
+  }
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    login(values).then(( data ) => {
-      if (data.token) {
-        localStorage.setItem("jwt", data.token);
-        setCurrentUser( data.user );  
-         resetForm();
-         onClose();
-      }
-    })
-    .catch(console.error);
+    login(values)
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem("jwt", data.token);
+          validateToken(data.token)
+          .then((user) => {
+    setCurrentUser(user);
+          setIsLoggedIn(true);
+          resetForm();
+          handleClose()
+          });
+        }
+      })
+      .catch(console.error);
   };
 
   return (
-    <div className={`form__modal ${isOpen ? "modal_is-opened" : ""}`}>
-      <div className="form__modal-container">
-        <h2 className="form__modal-title">Log In</h2>
+    <div className={`login__modal ${isOpen ? "modal_is-opened" : ""}`}>
+      <div className="login__modal-container">
+        <h2 className="login__modal-title">Log In</h2>
         <button
           type="button"
-          className="form__modal-close"
-          onClick={onClose}
+          className="login__modal-close"
+          onClick={handleClose}
         ></button>
-        <form onSubmit={handleFormSubmit} className="form__modal-inputs">
-          <label htmlFor="username" className="form__modal-label">
+        <form ref={formRef} onSubmit={handleFormSubmit} className="login__modal-inputs">
+          <label htmlFor="username" className="login__modal-label">
             Email
           </label>
           <input
             type="email"
-            className="form__modal-input"
+            className="login__modal-input"
             placeholder="Email"
             id="email"
             name="email"
             value={values.email}
             onChange={handleChange}
+            required
           />
-          <label htmlFor="image" className="form__modal-label">
+          <label htmlFor="image" className="login__modal-label">
             Password
           </label>
           <input
             type="password"
-            className="form__modal-input"
+            className="login__modal-input"
             placeholder="Password"
             id="password"
             name="password"
             value={values.password}
             onChange={handleChange}
+            required
           />
-          {imageError && <p className="form__modal-error">{imageError}</p>}
-          <button type="submit" className="form__modal-submit">
-            Login
-          </button>
-           <button type="button" className="form__modal-submit-signUp">
-            SignUp
-          </button>
+          <div className="login__modal-btns">
+            <button  disabled={!isValid} type="submit" className="login__modal-logIn">
+              Log in
+            </button>
+            <button onClick={handleRegister} type="button" className="login__modal-signUp">
+              Sign Up
+            </button>
+          </div>
         </form>
       </div>
     </div>
