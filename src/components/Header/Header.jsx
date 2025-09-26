ad4e799967427087221dfd35b50c4b34685530b2
adam wainkainen <awainikainen@gmail.com>
2025-09-26 18:11:19 -0400

    added dynamic nav bar for log in and sign up modals and avatar pic errors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,27 @@
 import logo from "../../images/logo.svg";
-import userLogo from "../../images/avatar.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-function Header({ handleAddGarmentModal, weatherData }) {
+function Header({
+  handleAddGarmentModal,
+  weatherData,
+  handleLoginModal,
+  handleSignUpModal,
+}) {
+  const { currentUser } = useContext(CurrentUserContext);
+  const [imageError, setImageError] = useState(false);
   const now = new Date();
   const dateStr = now.toLocaleDateString("default", {
     month: "long",
     day: "numeric",
   });
+
+  useEffect(() => {
+    setImageError(false);
+  }, [currentUser]);
 
   return (
     <header className="header">
@@ -23,13 +35,39 @@ function Header({ handleAddGarmentModal, weatherData }) {
         , {weatherData.city}
       </p>
       <ToggleSwitch />
-      <button onClick={handleAddGarmentModal} className="header__add-btn">
-        + Add Clothes
-      </button>
-      <Link className="header__link-profile" to={"/profile"}>
-        <p className="header__user">Terrance</p>
-        <img src={userLogo} alt="USER IMAGE" className="header__user-img" />
-      </Link>
+      {currentUser?.name ? (
+        <>
+          <button onClick={handleAddGarmentModal} className="header__add-btn">
+            + Add Clothes
+          </button>
+          <Link className="header__link-profile" to={"/profile"}>
+            <p className="header__user">{currentUser?.name}</p>
+            {imageError ? (
+              <div className="header__link-error">
+                <p className="header__link-error-name">t</p>
+              </div>
+            ) : (
+              <img
+              onError={() => {
+                setImageError(true)
+              }}
+                src={currentUser?.avatar}
+                alt="USER IMAGE"
+                className="header__user-img"
+              />
+            )}
+          </Link>
+        </>
+      ) : (
+        <>
+          <button onClick={handleSignUpModal} className="header__signUp">
+            Sign Up
+          </button>
+          <button onClick={handleLoginModal} className="header__logIn">
+            Log In
+          </button>
+        </>
+      )}
     </header>
   );
 }
