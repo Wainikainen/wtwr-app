c27d4a459d51716b0f0eb6d0d982f8408c9f3fb7
adam wainkainen <awainikainen@gmail.com>
2025-10-01 15:47:24 -0400

    refactored for cleaner and consistent style :art:

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,15 +1,15 @@
-import "./LoginModal.css";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { useForm } from "../../hooks/useForm";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 import { login, validateToken } from "../../utils/auth";
+import Modal from "../Modal/Modal";
+import "./LoginModal.css";
 
 function LoginModal({ isOpen, onClose, handleRegister }) {
   const { resetForm, values, handleChange, isValid, formRef } = useForm({
     email: "",
     password: "",
   });
-
   const { setCurrentUser, setIsLoggedIn } = useContext(CurrentUserContext);
 
   const handleClose = () => {
@@ -35,64 +35,56 @@ function LoginModal({ isOpen, onClose, handleRegister }) {
   };
 
   return (
-    <div className={`login__modal ${isOpen ? "modal_is-opened" : ""}`}>
-      <div className="login__modal-container">
-        <h2 className="login__modal-title">Log In</h2>
-        <button
-          type="button"
-          className="login__modal-close"
-          onClick={handleClose}
-        ></button>
-        <form
-          ref={formRef}
-          onSubmit={handleFormSubmit}
-          className="login__modal-inputs"
-        >
-          <label htmlFor="username" className="login__modal-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="login__modal-input"
-            placeholder="Email"
-            id="email"
-            name="email"
-            value={values.email}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="image" className="login__modal-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="login__modal-input"
-            placeholder="Password"
-            id="password"
-            name="password"
-            value={values.password}
-            onChange={handleChange}
-            required
-          />
-          <div className="login__modal-btns">
-            <button
-              disabled={!isValid}
-              type="submit"
-              className="login__modal-logIn"
-            >
-              Log in
-            </button>
-            <button
-              onClick={handleRegister}
-              type="button"
-              className="login__modal-signUp"
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <Modal isOpen={isOpen} onClose={handleClose} title="Log In">
+      <form
+        ref={formRef}
+        onSubmit={handleFormSubmit}
+        className="login__modal-inputs"
+      >
+        <label htmlFor="email" className="login__modal-label">
+          Email
+        </label>
+        <input
+          type="email"
+          className="login__modal-input"
+          placeholder="Email"
+          id="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="password" className="login__modal-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="login__modal-input"
+          placeholder="Password"
+          id="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+          required
+        />
+        <div className="login__modal-btns">
+          <button
+            disabled={!isValid}
+            type="submit"
+            className="login__modal-logIn"
+          >
+            Log in
+          </button>
+          <button
+            onClick={handleRegister}
+            type="button"
+            className="login__modal-signUp"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </Modal>
   );
 }
 
--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -1,7 +1,8 @@
-import "./RegisterModal.css";
+import React, { useContext } from "react";
 import { useForm } from "../../hooks/useForm";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
-import { useContext } from "react";
+import Modal from "../Modal/Modal";
+import "./RegisterModal.css";
 
 function RegisterModal({ isOpen, onClose, handleRegister, handleLoginModal }) {
   const { resetForm, values, handleChange, isValid, formRef } = useForm({
@@ -10,7 +11,6 @@ function RegisterModal({ isOpen, onClose, handleRegister, handleLoginModal }) {
     email: "",
     password: "",
   });
-
   const { setCurrentUser, setIsLoggedIn } = useContext(CurrentUserContext);
 
   const handleClose = () => {
@@ -28,90 +28,82 @@ function RegisterModal({ isOpen, onClose, handleRegister, handleLoginModal }) {
   };
 
   return (
-    <div className={`register__modal ${isOpen ? "modal_is-opened" : ""}`}>
-      <div className="register__modal-container">
-        <h2 className="register__modal-title">Sign Up</h2>
-        <button
-          type="button"
-          className="register__modal-close"
-          onClick={handleClose}
-        ></button>
-        <form
-          ref={formRef}
-          onSubmit={handleFormSubmit}
-          className="register__modal-inputs"
-        >
-          <label htmlFor="email" className="register__modal-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="register__modal-input"
-            placeholder="Email"
-            id="email"
-            name="email"
-            value={values.email}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="password" className="register__modal-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="register__modal-input"
-            placeholder="Password"
-            id="password"
-            name="password"
-            value={values.password}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="name" className="register__modal-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="register__modal-input"
-            placeholder="Name"
-            id="name"
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="avatar" className="register__modal-label">
-            Avatar
-          </label>
-          <input
-            type="text"
-            className="register__modal-input"
-            placeholder="Avatar"
-            id="avatar"
-            name="avatar"
-            value={values.avatar}
-            onChange={handleChange}
-            required
-          />
-          <div className="register__modal-btns">
-            <button
-              disabled={!isValid}
-              type="submit"
-              className="register__modal-signUp"
-            >
-              Sign Up
-            </button>
-            <button
-              onClick={handleLoginModal}
-              type="button"
-              className="register__modal-logIn"
-            >
-              Log In
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <Modal isOpen={isOpen} onClose={handleClose} title="Sign Up">
+      <form
+        ref={formRef}
+        onSubmit={handleFormSubmit}
+        className="register__modal-inputs"
+      >
+        <label htmlFor="email" className="register__modal-label">
+          Email
+        </label>
+        <input
+          type="email"
+          className="register__modal-input"
+          placeholder="Email"
+          id="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="password" className="register__modal-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="register__modal-input"
+          placeholder="Password"
+          id="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="name" className="register__modal-label">
+          Name
+        </label>
+        <input
+          type="text"
+          className="register__modal-input"
+          placeholder="Name"
+          id="name"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="avatar" className="register__modal-label">
+          Avatar
+        </label>
+        <input
+          type="text"
+          className="register__modal-input"
+          placeholder="Avatar"
+          id="avatar"
+          name="avatar"
+          value={values.avatar}
+          onChange={handleChange}
+          required
+        />
+        <div className="register__modal-btns">
+          <button
+            disabled={!isValid}
+            type="submit"
+            className="register__modal-signUp"
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={handleLoginModal}
+            type="button"
+            className="register__modal-logIn"
+          >
+            Log In
+          </button>
+        </div>
+      </form>
+    </Modal>
   );
 }
 
